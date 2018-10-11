4dc88874b78127e9ba359123d96beed5ec95e03f
songsong <huangsongsong@newhope-data.com>
2018-10-11 10:54:24 +0800

    删除wepy_test项目

--- test_wepy/src/utils/showToast.js
+++
@@ -1,8 +0,0 @@
-export default function showToast(mess, icon, that) {
-    //$invoke 一个页面或组件对另一个组件中的方法的直接调用，通过传入组件路径找到相应的组件，然后再调用其方法。
-    //参数：组件名, 方法名, 参数
-    that.$invoke("toast", 'show', {
-        title: mess,
-        img: `../icons/${icon}.png`
-    })
-}
